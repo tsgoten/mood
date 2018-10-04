75181baca412158529560cb8738da5ef07d390ac
Tarang <tsgoten@gmail.com>
2018-10-04 12:47:46 -0400

    added working button

--- mood/ViewController.swift
+++ mood/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func gifme() {
+        display.text = "hello"
+    }
+    
 
 
 }
